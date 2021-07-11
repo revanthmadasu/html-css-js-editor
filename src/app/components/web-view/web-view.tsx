02473a3f71d2ed12e06afe061a8fcaf835651038
Revanth Kumar <revanth.kumar@colortokens.com>
2021-07-11 18:38:09 +0530

    title for iframe

--- src/app/components/web-view/web-view.tsx
+++ src/app/components/web-view/web-view.tsx
@@ -5,7 +5,7 @@ function WebView(props: any) {
     return (
         <React.Fragment>
             <div className="code-editor-container w-100 h-100 py-1">
-                <iframe className="web-iframe w-100" srcDoc={renderDocument}></iframe>
+                <iframe className="web-iframe w-100" srcDoc={renderDocument} title="View Your changes here.."></iframe>
             </div>
         </React.Fragment>
     );
